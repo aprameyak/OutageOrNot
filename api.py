ab8d6f2990911cbde004a56786746cdf1e8c731d
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-03-25 18:41:39 -0400

    add model

--- api.py
+++ api.py
@@ -4,18 +4,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the trained model
 model = joblib.load('power_outage_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict_outage():
-    """
-    Receives processed data, makes predictions, and returns results.
-    """
     try:
         data = request.get_json()
         df = pd.DataFrame([data])
-        prediction = model.predict_proba(df)[:, 1].tolist() #probability of outage.
+        prediction = model.predict_proba(df)[:, 1].tolist() 
         return jsonify({'predictions': prediction})
 
     except Exception as e:
